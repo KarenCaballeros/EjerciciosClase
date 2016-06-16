f8b939b86b8e2ddd43fb4d0a167c5b2aee73af1d
KarenCaballeros <anakarencaballeros@gmail.com>
2016-06-15 20:21:49 -0600

    ejercicio resuelto

--- maximo.py
+++ maximo.py
@@ -1,12 +1,9 @@
 def maximo(lista):
-	if lista[0] > lista[1:]:
-		print("hola")
+	if len(lista) == 1:
 		return lista[0]
-	else:
-		return maximo(lista.remove(lista[0]))
 
-if lista[0:] > lista[1:]:
+	if lista[0] > maximo(lista[1:]):
 		return lista[0]
+
 	else:
-		lista[0] <= lista[1]
-		return maximo(lista[1:])			
+		return maximo(lista[1:])
